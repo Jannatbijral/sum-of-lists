502984c1d63335cdccbd1432d850f36d949b70ea
Jannatbijral <119727955+Jannatbijral@users.noreply.github.com>
2022-12-10 21:08:17 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,14 @@
-#program to print fibonacci series
 
-#defining a function
 def fib(n):
     a = 0
     b = 1
-#using conditional statements
+
     if n == 1:
         print(a)
     else:
         print(a)
         print(b)
-#using looping statements
+
         for i in range(2,n):
             c = a + b
             a = b
